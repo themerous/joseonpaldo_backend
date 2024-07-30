7160d5dd14a78d51acc6a1b4351390ee6c6ac88a
dongwoobae <https://github.com/dongwoobae/naver0314devops11.git>
2024-07-30 14:16:00 +0900

    commit

--- src/main/java/com/example/demo/data/contoller/AuthController.java
+++ src/main/java/com/example/demo/data/contoller/AuthController.java
@@ -3,6 +3,9 @@ package com.example.demo.data.contoller;
 import com.example.demo.JwtSetting.JwtTokenProvider;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/auth")
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
@@ -14,7 +17,13 @@ public class AuthController {
     }
 
     @GetMapping("/login")
-    public String authenticate(){
-        return "Login Page";
+    public Map<String,String> authenticate(@RequestParam(name = "authToken",required = false) String authToken){
+        Map<String,String> map = new HashMap<>();
+        if(authToken == null){
+            map.put("loginStatus","notAuthenticated");
+        }else{
+            map.put("loginStatus","authenticated");
+        }
+        return map;
     }
 }
